8c193dd17552ec5b891190f88509c966b696fc89
Oleg <30oleg.zv94@gmail.com>
2022-03-05 00:07:20 +0300

    add menu burger

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,9 @@
 const $subtitle = document.querySelector('.header__subtitle p');
 const text = $subtitle.textContent;
 
+const $burger = document.querySelector('.header__burger');
+const $menu = document.querySelector('.header-menu');
+
 let counter = 0;
 
 let timer = setTimeout(function showText() {
@@ -13,3 +16,21 @@ let timer = setTimeout(function showText() {
 }, 0);
 
 // console.log(left);
+
+$burger.addEventListener('click', (e) => {
+  showMenu(e);
+});
+
+function showMenu(e) {
+  let burger = e.target.closest('.header__burger');
+
+  if (burger.classList.contains('active')) {
+    $menu.style.left = `-100%`;
+    burger.classList.remove('active');
+    console.log('yes');
+  } else if (burger) {
+    $menu.style.left = 0;
+    burger.classList.add('active');
+    console.log('yes');
+  }
+}
